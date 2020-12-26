566c482e99a361477304f3dc00cdfef6ae363854
Roshanak Agahi <rosh.agahi@gmail.com>
2020-12-26 21:01:22 +0000

    Automatically backed up by Learn

--- db/migrate/20201226205630_create_dogs.rb
+++ db/migrate/20201226205630_create_dogs.rb
@@ -11,4 +11,13 @@ class CreateDogs < ActiveRecord::Migration[5.2]
     drop_table :dogs 
   end
   
+  # change method is a shorter way of writing up and down methods. refactor to just this:
+  
+  # def change
+  #  create_table :dogs do |t|
+  #    t.string :name 
+  #    t.string :breed 
+  #  end
+  # end
+  
 end
